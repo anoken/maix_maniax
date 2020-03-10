9538f085b4ced14de2538a794d2c183476a0e12d
anoken <31365453+anoken@users.noreply.github.com>
2020-03-10 22:04:10 +0900

    Add files via upload

--- 04_kmodel_train/02_yolov2_train/yolo_keras_train.py
+++ 04_kmodel_train/02_yolov2_train/yolo_keras_train.py
@@ -6,44 +6,31 @@ import keras.backend as K
 import tensorflow as tf
 import numpy as np
 import os, cv2
-from yolo_utils.annotation import get_train_annotations, get_unique_labels
+from yolo_utils.annotation import get_train_annotations
 from yolo_utils.batch_gen import create_batch_generator
 from yolo_utils.loss import YoloLoss
 from mobilenet_sipeed.mobilenet_v1 import MobileNet
 
-LABELS = ['object1']
+LABELS = ["Target"]			#オブジェクトの名前
 
-IMAGE_H, IMAGE_W = 224, 224
-GRID_H,GRID_W= 7,7
-CLASS            = len(LABELS)
-CLASS_WEIGHTS    = np.ones(CLASS, dtype='float32')
-OBJ_THRESHOLD    = 0.3
-NMS_THRESHOLD    = 0.3
+IMAGE_H, IMAGE_W = 224, 224		#画像のサイズ
+GRID_H,  GRID_W  = 7 , 7		#確率MAPのサイズ
+CLASS            = len(LABELS)		#検出オブジェクトの数
+CLASS_WEIGHTS    = np.ones(CLASS, dtype='float32')#検出オブジェクトの確率
+#アンカーボックス
 ANCHORS          = [0.57273, 0.677385, 1.87446, 2.06253, 3.33843,
-			 5.47434, 7.88282, 3.52778, 9.77052, 9.16828]
-BOX              = 5
+			 5.47434, 7.88282, 3.52778, 9.77052, 9.16828]	
+BOX              = int(len(ANCHORS)/2)		#アンカーボックスの大きさ
+BATCH_SIZE       = 16				#学習データを小分けにする数
 
-NO_OBJECT_SCALE  = 1.0
-OBJECT_SCALE     = 5.0
-COORD_SCALE      = 1.0
-CLASS_SCALE      = 1.0
-
-BATCH_SIZE       = 8
-TRUE_BOX_BUFFER  = 50
-
-train_times=4
-valid_times=1
-jitter=True
-is_only_detect=False
-
+#学習に使うデータセットの格納場所
 train_image_folder = './Dataset/image/train/'
 train_annot_folder = './Dataset/annotation/train/'
 valid_image_folder = './Dataset/image/test/'
 valid_annot_folder = './Dataset/annotation/test/'
 
-#MobileNet-YOLOv2のモデルを作成
+#MobileNetベースのYOLOv2のモデルを作成
 input_image = Input(shape=(IMAGE_H, IMAGE_W, 3))
-true_boxes  = Input(shape=(1, 1, 1, TRUE_BOX_BUFFER , 4))
 
 mobilenet = MobileNet(input_shape=(IMAGE_H,IMAGE_W,3),
  alpha = 0.75,depth_multiplier = 1, dropout = 0.001, include_top=False,
@@ -68,28 +55,36 @@ layer.set_weights([new_kernel, new_bias])
 
 model.summary()
 
-def normalize(image):
-    image = image / 255.
-    image = image - 0.5
-    image = image * 2.
-    return image
-
 #画像とアノテーションをバッチ化
+
+train_times=4	#学習を繰り返す回数
+valid_times=1	#訓練を繰り返す回数
+jitter=True	#学習画像をX,Y方向に動かしてノイズを与える
+
 train_annotations, valid_annotations = get_train_annotations(LABELS,
                             train_image_folder,train_annot_folder,
-                            valid_image_folder,valid_annot_folder,
-                               is_only_detect)
+                            valid_image_folder,valid_annot_folder)
 
 train_batch_generator = create_batch_generator(train_annotations,
-                         IMAGE_H, GRID_H,BATCH_SIZE,ANCHORS,
+                         IMAGE_H,  GRID_H,BATCH_SIZE,ANCHORS,
                          train_times,jitter=jitter,norm=normalize)
                                          
 valid_batch_generator = create_batch_generator(valid_annotations,
                          IMAGE_H,  GRID_H,BATCH_SIZE,ANCHORS,
-                         valid_times,jitter=jitter,norm=normalize)
+                         train_times,jitter=jitter,norm=normalize)
 
 #損失関数と最適化アルゴリズムの設定
-yolo_loss = YoloLoss(GRID_H,CLASS,ANCHORS,COORD_SCALE,CLASS_SCALE,
+
+#アンカーボックスのオブジェクトのある部分の予測誤差にペナルティを科す係数
+NO_OBJECT_SCALE  = 1.0	
+#アンカーボックスのオブジェクトのない部分の予測誤差にペナルティを科す係数
+OBJECT_SCALE     = 5.0	
+#位置とサイズの予測（x、y、w、h）の誤差にペナルティを科す係数
+COORD_SCALE      = 1.0	
+#クラス予測の誤差にペナルティを科す係数
+CLASS_SCALE      = 1.0	
+
+yolo_loss = YoloLoss(GRID,CLASS,ANCHORS,COORD_SCALE,CLASS_SCALE,
                      OBJECT_SCALE,NO_OBJECT_SCALE)
 custom_loss = yolo_loss.custom_loss(BATCH_SIZE)
 
@@ -113,11 +108,12 @@ output_layer = model.layers[2].name+'/BiasAdd'
 model.save("my_yolo.h5",include_optimizer=False)
 
 #Keras->TensorFlowLite形式に変換
-converter = tf.lite.TFLiteConverter.from_keras_model_file("my_yolo.h5",output_arrays=[output_layer])
-tflite_model = converter.convert()
+converter = tf.lite.TFLiteConverter.from_keras_model_file("my_yolo.h5",
+output_arrays=[output_layer])tflite_model = converter.convert()
 open('my_yolo.tflite', "wb").write(tflite_model)
 
 #TensorFlowLite->kmodel形式に変換
 import subprocess
-subprocess.run(['./ncc/ncc','my_mbnet.tflite','my_mbnet.kmodel'
+subprocess.run(['./ncc/ncc','my_yolo.tflite','my_yolo.kmodel'
 ,'-i','tflite','-o','k210model','--dataset','images'])
+
