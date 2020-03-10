70ca02fda1f165bbf704b3a295d08b0535c46376
anoken <31365453+anoken@users.noreply.github.com>
2020-03-11 00:03:19 +0900

    Add files via upload

--- 04_kmodel_train/02_yolov2_train/yolo_keras_train.py
+++ 04_kmodel_train/02_yolov2_train/yolo_keras_train.py
@@ -1,3 +1,4 @@
+## Copyright (c) 2019 aNoken
 import keras
 from keras.models import Model
 from keras.layers import Reshape,  Conv2D, Input
@@ -11,7 +12,7 @@ from yolo_utils.batch_gen import create_batch_generator
 from yolo_utils.loss import YoloLoss
 from mobilenet_sipeed.mobilenet_v1 import MobileNet
 
-LABELS = ["Target"]			#オブジェクトの名前
+LABELS = ["target"]			#オブジェクトの名前
 
 IMAGE_H, IMAGE_W = 224, 224		#画像のサイズ
 GRID_H,  GRID_W  = 7 , 7		#確率MAPのサイズ
@@ -26,8 +27,8 @@ BATCH_SIZE       = 16				#学習データを小分けにする数
 #学習に使うデータセットの格納場所
 train_image_folder = './Dataset/image/train/'
 train_annot_folder = './Dataset/annotation/train/'
-valid_image_folder = './Dataset/image/test/'
-valid_annot_folder = './Dataset/annotation/test/'
+valid_image_folder = ''
+valid_annot_folder = ''
 
 #MobileNetベースのYOLOv2のモデルを作成
 input_image = Input(shape=(IMAGE_H, IMAGE_W, 3))
@@ -57,21 +58,22 @@ model.summary()
 
 #画像とアノテーションをバッチ化
 
-train_times=4	#学習を繰り返す回数
+train_times=1	#学習を繰り返す回数
 valid_times=1	#訓練を繰り返す回数
 jitter=True	#学習画像をX,Y方向に動かしてノイズを与える
 
-train_annotations, valid_annotations = get_train_annotations(LABELS,
-                            train_image_folder,train_annot_folder,
-                            valid_image_folder,valid_annot_folder)
+def normalize(image): 
+    image = image / 255. 
+    image = image - 0.5 
+    image = image * 2. 
+    return image
+    
+    
+train_annotations, valid_annotations = get_train_annotations(LABELS,train_image_folder,train_annot_folder,valid_image_folder,valid_annot_folder)
 
-train_batch_generator = create_batch_generator(train_annotations,
-                         IMAGE_H,  GRID_H,BATCH_SIZE,ANCHORS,
-                         train_times,jitter=jitter,norm=normalize)
+train_batch_generator = create_batch_generator(train_annotations,IMAGE_H,GRID_H,BATCH_SIZE,ANCHORS,train_times,jitter=jitter,norm=normalize)
                                          
-valid_batch_generator = create_batch_generator(valid_annotations,
-                         IMAGE_H,  GRID_H,BATCH_SIZE,ANCHORS,
-                         train_times,jitter=jitter,norm=normalize)
+valid_batch_generator = create_batch_generator(valid_annotations,IMAGE_H,GRID_H,BATCH_SIZE,ANCHORS,train_times,jitter=jitter,norm=normalize)
 
 #損失関数と最適化アルゴリズムの設定
 
@@ -84,12 +86,10 @@ COORD_SCALE      = 1.0
 #クラス予測の誤差にペナルティを科す係数
 CLASS_SCALE      = 1.0	
 
-yolo_loss = YoloLoss(GRID,CLASS,ANCHORS,COORD_SCALE,CLASS_SCALE,
+yolo_loss = YoloLoss(GRID_H,CLASS,ANCHORS,COORD_SCALE,CLASS_SCALE,
                      OBJECT_SCALE,NO_OBJECT_SCALE)
 custom_loss = yolo_loss.custom_loss(BATCH_SIZE)
-
 optimizer = Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
-
 model.compile(loss=custom_loss, optimizer=optimizer)
 
 # 学習開始
@@ -97,9 +97,11 @@ history=model.fit_generator(generator        = train_batch_generator,
                     steps_per_epoch  = len(train_batch_generator), 
                     validation_data  = valid_batch_generator,
                     validation_steps = len(valid_batch_generator),
-                    epochs           = 30, 
+                    epochs           = 50, 
                     verbose          = 1,
                     max_queue_size   = 3)
+
+
 
 #Kerasモデル形式で保存
 output_node_names = [node.op.name for node in model.outputs]
@@ -108,8 +110,8 @@ output_layer = model.layers[2].name+'/BiasAdd'
 model.save("my_yolo.h5",include_optimizer=False)
 
 #Keras->TensorFlowLite形式に変換
-converter = tf.lite.TFLiteConverter.from_keras_model_file("my_yolo.h5",
-output_arrays=[output_layer])tflite_model = converter.convert()
+converter = tf.lite.TFLiteConverter.from_keras_model_file("my_yolo.h5",output_arrays=[output_layer])
+tflite_model = converter.convert()
 open('my_yolo.tflite', "wb").write(tflite_model)
 
 #TensorFlowLite->kmodel形式に変換
